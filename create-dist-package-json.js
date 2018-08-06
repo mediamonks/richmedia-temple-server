05a43a6acd7fb8adb068aceda4cdaf5559ece897
Mient-jan Stelling <mientjan.stelling@gmail.com>
2018-08-06 15:47:26 +0200

    refactoring code so works with travis

--- create-dist-package-json.js
+++
@@ -1,8 +0,0 @@
-const json = require('./package.json');
-const fs = require('fs');
-
-const distPackage = Object.assign({}, json);
-distPackage.scripts = {};
-distPackage.devDependencies = {};
-
-fs.writeFile('./dist/package.json', JSON.stringify(distPackage), { encoding: 'utf8' });
