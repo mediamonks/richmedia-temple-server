7b7dc99382045690c646d21ecc15ef3ae7d781dc
Mient-jan Stelling <mientjan.stelling@gmail.com>
2018-08-16 15:25:01 +0200

    refactoring configuration for restructered way of working.

--- src/cli/dev.js
+++ src/cli/dev.js
@@ -1,37 +1,3 @@
-const findJSONConfigs = require('../util/findRichmediaRC');
-const configGeneratorByRichmediarcList = require('../webpack/config/createConfigByRichmediarcList');
-const devServer = require('../dev-server');
-const inquirer = require('inquirer');
+const dev = require('../dev');
 
-module.exports = function dev(allConfigsSelector = './**/.richmediarc') {
-  findJSONConfigs(allConfigsSelector, ['settings.entry.js', 'settings.entry.html']).then(
-    configs => {
-      const questions = [];
-
-      if (configs.length > 0) {
-        questions.push({
-          type: 'list',
-          name: 'devLocation',
-          message: 'Please choose the current build to start developing.',
-          choices: ['ALL', ...configs.map(({ location }) => location)],
-        });
-      }
-
-      inquirer.prompt(questions).then(answers => {
-        let configsResult = null;
-
-        if (answers.devLocation === 'ALL') {
-          configsResult = configs;
-        } else {
-          configsResult = configs.filter(({ location }) => location === answers.devLocation);
-        }
-
-        configGeneratorByRichmediarcList(configsResult, 'development').then(result => {
-          const list = result.map((webpack, index) => ({ webpack, rc: configsResult[index] }));
-
-          devServer(list);
-        });
-      });
-    },
-  );
-};
+dev();
