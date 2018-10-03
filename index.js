171aa45c4313412d3f1022c7d9935d60cf951c9b
Mient-jan Stelling <mientjan.stelling@gmail.com>
2018-10-03 16:08:35 +0200

    adding ability to download a screenshot of the richmedia unit when clicking on screenshot.
    @todo adding ability to specify procedure to make a screenshot.

---
+++ index.js
@@ -0,0 +1,5 @@
+
+
+module.exports.dev = require('./src/dev');
+module.exports.build = require('./src/build');
+module.exports.screenshot = require('./src/screenshot');
--- src/dev-server.js
+++ src/dev-server.js
@@ -5,7 +5,10 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const express = require('express');
 const handlebars = require('handlebars');
-const portscanner = require('portscanner');
+const portfinder = require('portfinder');
+const screenshot = require('@mediamonks/richmedia-temple-screenshot');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
 
 const templatePromise = Promise.resolve(true).then(
   () =>
@@ -33,53 +36,66 @@ function getNameFromSetting(setting) {
  *
  * @param {Array<{webpack: *, settings: {location, data}}>} configs
  */
-module.exports = function devServer(configs) {
+module.exports = async function devServer(configs) {
   const webpackConfigList = configs.map(({ webpack }) => webpack);
   const settingList = configs.map(({ settings }) => settings);
+  const port = await portfinder.getPortPromise();
+  const template = await templatePromise;
 
-  templatePromise.then(template => {
-    const app = express();
+  const app = express();
 
-    webpackConfigList.forEach((config, index) => {
-      const compiler = webpack(config);
+  webpackConfigList.forEach((config, index) => {
+    const compiler = webpack(config);
 
-      app.use(
-        webpackDevMiddleware(compiler, {
-          publicPath: `/${getNameFromSetting(settingList[index])}/`,
-        }),
-      );
-      app.use(webpackHotMiddleware(compiler));
-    });
-
-
-    app.get('/', (req, res) => {
-      const templateConfig = {
-        banner: settingList.map(value => {
-          const name = getNameFromSetting(value);
-
-          return {
-            src: `./${name}/`,
-            name,
-            width: value.data.settings.size.width,
-            height: value.data.settings.size.height,
-          };
-        }),
-      };
-
-      res.send(template(templateConfig));
-    });
-
-    portscanner.findAPortNotInUse(3000, 3010, '127.0.0.1', function(error, port) {
-      app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
-    });
-
-    // eslint-disable-next-line
-
-    process.on('uncaughtException', e => {
-      // eslint-disable-next-line
-      console.log(e);
-      // app.close();
-    });
-    process.on('SIGTERM', () => app.close());
+    app.use(
+      webpackDevMiddleware(compiler, {
+        publicPath: `/${getNameFromSetting(settingList[index])}/`,
+      }),
+    );
+    app.use(webpackHotMiddleware(compiler));
   });
+
+  app.get('/', (req, res) => {
+    const templateConfig = {
+      banner: settingList.map(value => {
+        const name = getNameFromSetting(value);
+
+        return {
+          src: `./${name}/`,
+          name,
+          width: value.data.settings.size.width,
+          height: value.data.settings.size.height,
+        };
+      }),
+    };
+
+    res.send(template(templateConfig));
+  });
+
+  app.get('/screenshot/:target', (req, res) => {
+    const location = path.join(__dirname, '../tempfolder', 'screenshot.png');
+
+    screenshot
+      .fromUrl({
+        config: {},
+        url: `http://localhost:${port}/${req.params.target}/`,
+        location,
+      })
+      .then(() => readFile(location))
+      .then(img => {
+        res.contentType('image/png');
+        res.end(img, 'binary');
+      });
+  });
+
+  app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
+
+  // eslint-disable-next-line
+
+  process.on('uncaughtException', e => {
+    // eslint-disable-next-line
+    console.log(e);
+    // app.close();
+  });
+  process.on('SIGTERM', () => app.close());
 };
