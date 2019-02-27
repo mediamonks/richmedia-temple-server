1481ff7e0fb4592dea20c96f633e0aaea436eff6
Mient-jan Stelling <mientjan.stelling@gmail.com>
2019-02-27 17:01:20 +0100

    generating correct backup.json

--- src/util/convert/richmediaRCToMonetBackupJSON.js
+++ src/util/convert/richmediaRCToMonetBackupJSON.js
@@ -6,6 +6,14 @@ module.exports = function richmediaRCToMonetBackupJSON({ content }) {
 
   result.rootAssets = Object.keys(content).reduce((prev, name) => {
     const value = content[name];
+
+    if(value.type === 'monetVideo'){
+      value.type = 'video';
+      value.url = value.video.url;
+
+      delete value.video
+    }
+
     prev[`${value.type}.${name}`] = value;
     return prev;
   }, {});
