aa5d15739eaa443842186718854e1fe31ac6974d
Mient-jan Stelling <mientjan.stelling@gmail.com>
2020-08-17 23:53:05 +0200

    adding rm plugin, and extra fixes

--- src/webpack/plugin/RichmediaRCPlugin.js
+++ src/webpack/plugin/RichmediaRCPlugin.js
@@ -18,65 +18,41 @@ class RichmediaRCPlugin {
     compiler.hooks.emit.tapPromise('RichmediaRCPlugin', async compilation => {
       const json = await getRichmediaRC(this.config.filepath);
 
+      const all = [];
       leafs(json.content, (value, obj, name) => {
-        if(isFile(value) && !isExternalURL(value)){
+        if (isFile(value) && !isExternalURL(value)) {
+          all.push(new Promise(resolve => {
 
+            fs.readFile(value, (err, source) => {
+              const data = path.parse(value);
+              obj[name] = `${md5File.sync(value)}${data.ext}`;
+              compilation.assets[obj[name]] = {
+                source: () => source,
+                size() {
+                  return Buffer.byteLength(this.source());
+                },
+              };
+
+              resolve(compilation);
+            });
+          }));
         }
       });
 
-      if (json.content) {
-        const result = Object.keys(json.content).map(key => {
-          const value = json.content[key];
+      await Promise.all(result);
+      const result = JSON.stringify(json);
 
-          if ((value.type === 'image' || value.type === 'video') && !isExternalURL(value.url)) {
-            const url = path.join(path.dirname(this.config), value.url);
-            const data = path.parse(url);
-            value.url = `${md5File.sync(url)}${data.ext}`;
-
-            return new Promise(resolve => {
-              fs.readFile(url, (err, source) => {
-                compilation.assets[value.url] = {
-                  source: () => source,
-                  size() {
-                    return Buffer.byteLength(this.source());
-                  },
-                };
-
-                resolve(compilation);
-              });
-            });
-          }
-
-          return null;
-        });
-
-        await Promise.all(result);
-        const backupResult = JSON.stringify(
-          richmediaRCToMonetBackupJSON(JSON.parse(JSON.stringify(json))),
-        );
-        const manifestResult = JSON.stringify(richmediaRCToMonetManifestJSON(json));
-
-        // Insert this list into the Webpack build as a new file asset:
-        compilation.assets['backup.json'] = {
-          source: () => backupResult,
-          size() {
-            return this.source().length;
-          },
-        };
-
-        compilation.assets['manifest.json'] = {
-          source: () => manifestResult,
-          size() {
-            return this.source().length;
-          },
-        };
-
-        return compilation;
-      }
+      // Insert this list into the Webpack build as a new file asset:
+      compilation.assets['.richmediarc'] = {
+        source: () => result,
+        size() {
+          return this.source().length;
+        },
+      };
 
       return compilation;
     });
   }
 }
 
-module.exports = MonetJSONPlugin;
+module.exports = RichmediaRCPlugin;
