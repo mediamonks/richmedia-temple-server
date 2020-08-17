ea6a6256bfb8055b66cf8e6971256c665d8f7ead
Mient-jan Stelling <mientjan.stelling@gmail.com>
2020-08-17 23:12:04 +0200

    adding js variables to css

--- src/webpack/loader/RichmediaRCLoader.js
+++ src/webpack/loader/RichmediaRCLoader.js
@@ -1,39 +1,44 @@
 const loaderUtils = require('loader-utils');
 const isExternalURL = require('../../util/isExternalURL');
+const leafs = require("../../util/leafs");
+const isFile = require("../../util/isFile");
 
 /**
  * Allows you to import external files into a json value.
  * Can be used for any value, in an object or array.
  */
 module.exports = function RichmediaRCLoader(data) {
+  console.log('RichmediaRCLoader', data);
+
   this.cacheable();
   const loaderContext = this;
-  let content = typeof data === 'string' ? JSON.parse(data) : data;
+  data = typeof data === 'string' ? JSON.parse(data) : data;
 
   let ruuid = Date.now();
   const replaceItems = [];
 
-  if (content && content.content) {
-    Object.keys(content.content).forEach(key => {
-      const item = content.content[key];
-      if ((item.type === 'video' || item.type === 'image') && !isExternalURL(item.url)) {
-        ruuid += 1;
+  if (data && data.content) {
+    leafs(data.content, (value, obj, name) => {
+      if(isFile(value) && !isExternalURL(value)){
         const id = `uuid_replace_${ruuid.toString(16)}`;
+
         replaceItems.push({
           key: loaderUtils.stringifyRequest(loaderContext, id),
-          value: `require(${loaderUtils.stringifyRequest(loaderContext, `${item.url}`)})`,
+          value: `require(${loaderUtils.stringifyRequest(loaderContext, `${value}`)})`,
         });
 
-        item.url = id;
+        obj[name] = id;
       }
     });
   }
 
-  content = JSON.stringify(content)
+  console.log({data});
+
+  data = JSON.stringify(data)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
 
-  content = replaceItems.reduce((prev, item) => prev.replace(item.key, item.value), content);
+  data = replaceItems.reduce((prev, item) => prev.replace(item.key, item.value), data);
 
-  return `module.exports = ${content};`;
+  return `module.exports = ${data};`;
 };
