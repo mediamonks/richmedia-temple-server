8bbd98e55a44c2908c0538505719037e91ef79b6
Mient-jan Stelling <mientjan.stelling@gmail.com>
2020-08-31 15:38:01 +0200

    adding extra compatible file's

--- src/util/isFile.js
+++ src/util/isFile.js
@@ -1,4 +1,4 @@
-const r = /(\w+\.(jpg|png|gif|eps|json|js|html|css)$)|\.(richmediarc|richmediarc\.\w+)$/;
+const r = /(\w+\.(jpg|png|gif|eps|json|js|html|css|svg|mp4|text|md|woff|woff2|otf)$)|\.(richmediarc|richmediarc\.\w+)$/;
 
 module.exports = function isFile(val){
   return r.test(val);
